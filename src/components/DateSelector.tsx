ac296a6a2f7edd45f8332386037e1f3067156c04
429 <a429@MacBook-Pro.local>
2025-06-11 22:28:38 +0300

    has styled datePicker

--- src/components/DateSelector.tsx
+++ src/components/DateSelector.tsx
@@ -1,16 +1,24 @@
 import React,{useState} from 'react'
 import DatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css";
+import '../css/dateSelector.css';
+
 
 
 const DateSelector: React.FC = () => {
     const [inspectDate,setInspectDate] = useState<Date | null>(new Date());
 
     return(
-        <DatePicker
+        <div>
+            <h3>інформація оновлена</h3>
+               <DatePicker
+               wrapperClassName='date-picker'
         selected={inspectDate}
-        onChange={(date:Date)=> setInspectDate(date) }
+        onChange={(date:Date|null)=> setInspectDate(date) }
         dateFormat="dd/MM/yyyy"
         />
+        </div>
+     
     )
 }
 
